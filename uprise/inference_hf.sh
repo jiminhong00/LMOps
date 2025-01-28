bc72d8ff1c31260bf66eab30cdbb22a6ea91b812
JHong <jiminhong00@snu.ac.kr>
2025-01-28 17:21:32 +0900

    inference code modified

--- uprise/inference_hf.sh
+++ uprise/inference_hf.sh
@@ -1,5 +1,5 @@
 #export TASK=$1 # task name for evaluation, should be the same as the name in the task.py file
-export TASK=openbookqa
+export TASK=rte 
 export LLM="EleutherAI/gpt-neo-2.7B" # LLM for inference
 export CACHE_DIR=$PWD/cache # directory path for caching the LLM checkpoints, task datasets, etc.
 export RETRIEVER=$PWD/downloads/retriever_ckpt.2 # path to the downloaded retriever checkpoint
@@ -8,18 +8,18 @@ export NUM_PROMPTS=1 #original setting of 3
 
 # retrieve prompts for each task example
 # the retrieved prompts will be in '$PWD/my_data/experiment/uprise/${TASK}_prompts.json'
-# CUDA_VISIBLE_DEVICES=0,1,2,3 python DPR/dense_retriever.py \
-# 	 model_file=${RETRIEVER} \
-# 	 qa_dataset=qa_uprise ctx_datatsets=[dpr_uprise] \
-# 	 encoded_ctx_files=[$PWD/my_data/experiment/uprise/dpr_enc_index_*] \
-# 	 out_file=$PWD/my_data/experiment/uprise/${TASK}_prompts_${NUM_PROMPTS}shot.json \
-# 	 datasets.qa_uprise.task_name=${TASK} \
-# 	 datasets.qa_uprise.cache_dir=${CACHE_DIR} \
-# 	 n_docs=${NUM_PROMPTS} \
-# 	 ctx_sources.dpr_uprise.prompt_pool_path=${PROMPT_POOL} \
-# 	 ctx_sources.dpr_uprise.prompt_setup_type=qa \
-# 	 encoder.cache_dir=${CACHE_DIR} \
-# 	 hydra.run.dir=$PWD/my_data/experiment/uprise
+CUDA_VISIBLE_DEVICES=0,1,2,3 python DPR/dense_retriever.py \
+	 model_file=${RETRIEVER} \
+	 qa_dataset=qa_uprise ctx_datatsets=[dpr_uprise] \
+	 encoded_ctx_files=[$PWD/my_data/experiment/uprise/dpr_enc_index_*] \
+	 out_file=$PWD/my_data/experiment/uprise/${TASK}_prompts_${NUM_PROMPTS}shot.json \
+	 datasets.qa_uprise.task_name=${TASK} \
+	 datasets.qa_uprise.cache_dir=${CACHE_DIR} \
+	 n_docs=${NUM_PROMPTS} \
+	 ctx_sources.dpr_uprise.prompt_pool_path=${PROMPT_POOL} \
+	 ctx_sources.dpr_uprise.prompt_setup_type=qa \
+	 encoder.cache_dir=${CACHE_DIR} \
+	 hydra.run.dir=$PWD/my_data/experiment/uprise
 
 # CUDA_VISIBLE_DEVICES=0,1,2,3 python DPR/dense_retriever.py \
 # 	 model_file=${RETRIEVER} \
@@ -53,15 +53,15 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch --num_processes 1 --main_process_
 
 # # run UPRISE
 # # the LLM predictions will be in '$PWD/my_data/experiment/uprise/${TASK}_Uprise_pred_${NUM_PROMPTS}shot.json'
-# CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch --num_processes 1 --main_process_port \
-# 	 23548       inference.py \
-# 	 prompt_file=$PWD/my_data/experiment/uprise/${TASK}_prompts_${NUM_PROMPTS}shot.json \
-# 	 task_name=${TASK} \
-# 	 output_file=$PWD/my_data/experiment/uprise/${TASK}_Uprise_pred_${NUM_PROMPTS}shot.json \
-# 	 res_file=$PWD/my_data/experiment/uprise/${TASK}_evaluation_res.txt \
-# 	 model_name=${LLM} cache_dir=${CACHE_DIR} \
-# 	 num_prompts=${NUM_PROMPTS}  batch_size=4 \
-# 	 hydra.run.dir=$PWD/my_data/experiment/uprise
+CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch --num_processes 1 --main_process_port \
+	 23548       inference.py \
+	 prompt_file=$PWD/my_data/experiment/uprise/${TASK}_prompts_${NUM_PROMPTS}shot.json \
+	 task_name=${TASK} \
+	 output_file=$PWD/my_data/experiment/uprise/${TASK}_Uprise_pred_${NUM_PROMPTS}shot.json \
+	 res_file=$PWD/my_data/experiment/uprise/${TASK}_evaluation_res.txt \
+	 model_name=${LLM} cache_dir=${CACHE_DIR} \
+	 num_prompts=${NUM_PROMPTS}  batch_size=4 \
+	 hydra.run.dir=$PWD/my_data/experiment/uprise
 
 # CUDA_VISIBLE_DEVICES=0,1,2,3  accelerate launch --num_processes 1 --main_process_port \
 # 	 23548       inference.py \
